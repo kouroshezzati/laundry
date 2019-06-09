c5262e931f784e4565c4b5afe8cecfcd1db06c1f
moslem ezzati <moslem.ezati@gmail.com>
2019-06-09 17:10:18 +0430

    add flags to language instead of words

--- src/components/Languages/LanguageComponent.js
+++ src/components/Languages/LanguageComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { translate } from 'react-i18next';
 import './styles.css';
+import enImg from '../../assets/images/united-kingdom.png'
+import frImg from '../../assets/images/france.png'
 
 export class LanguageComponent extends Component {
   changeLanguageTo = lang => {
@@ -15,14 +17,14 @@ export class LanguageComponent extends Component {
           className={i18n.language === 'en-US' ? 'active-lang' : ''}
           onClick={e => this.changeLanguageTo('en-US')}
         >
-          En
+          <img alt="laundry" src={enImg} />
         </span>
-        |
+        {' '}
         <span
           className={i18n.language === 'fr-FR' ? 'active-lang' : ''}
           onClick={e => this.changeLanguageTo('fr-FR')}
         >
-          Fr
+          <img alt="laundry" src={frImg} />
         </span>
       </div>
     );
