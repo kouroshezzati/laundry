c0fb283a5d1c05fc858aa6a3d2f2c1c2be3a2ea2
moslem ezzati <moslem.ezati@gmail.com>
2019-06-21 17:37:55 +0430

    add ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps : [{
-    name: 'Laundry_web_app',
-    script: 'npx',
+    name: 'laundry',
+    script: 'app.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'serve build -s',
+    args: 'one two',
     instances: 1,
     autorestart: true,
     watch: false,
@@ -18,13 +18,13 @@ module.exports = {
   }],
 
   deploy : {
-    development : {
-      user : 'root',
-      host : '145.131.3.166',
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
       ref  : 'origin/master',
-      repo : '/home/git/laundry.git',
-      path : '/root/laundry/',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env development'
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
