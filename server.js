a5b99ae7a883b616e21376aa346c1af2730dad84
moslem ezzati <moslem.ezati@gmail.com>
2019-06-25 19:00:10 +0430

    server resolve all files to index.html

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-app.use('/static', express.static(__dirname + '/static'));
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 const PORT = process.env.PORT || 3001;
