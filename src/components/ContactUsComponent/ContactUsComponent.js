9c63c0a0c95cca7e69384851013a064464961d69
namin <mokhtari69r@gmail.com>
2019-07-08 03:13:49 -0700

    add resource img for footer

--- src/components/ContactUsComponent/ContactUsComponent.js
+++ src/components/ContactUsComponent/ContactUsComponent.js
@@ -147,7 +147,7 @@ export const ContactInfo = props => {
       <div className="contact-record">
         <span className="key">{t('Kvk nummer')}:</span>
         <span className="value">74639757</span><br></br>
-        <span className="key">{t('B.T.W Nummer')}:</span>
+        <span className="key">{t('BTW Nummer')}:</span>
         <span className="value">NL 072877054B02</span><br></br>
         <span className="value">Email: Info@bubblesonline.nl</span>
       </div>
