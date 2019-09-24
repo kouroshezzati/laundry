9f6a00fcf840e289baa243001245d580e91f912e
moslem ezzati <moslem.ezati@gmail.com>
2019-09-24 22:18:07 +0330

    change slider to business section

--- src/components/Business/BusinessComponent.js
+++ src/components/Business/BusinessComponent.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react';
 import { translate } from 'react-i18next';
 import { Button } from '@material-ui/core';
 import './style.css';
-import galoo from '../ServicesComponent/img/galoo.jpg';
 import { NavLink } from 'react-router-dom';
-
+import { Slide } from 'react-slideshow-image';
+import galoo from '../ServicesComponent/img/galoo.jpg';
+import coat from './img/coat.jpg';
+import fabrics from './img/fabrics.jpg';
+import rags from './img/rags.jpg';
+import towels from './img/towels.jpg';
+const slideImages = [galoo, coat, fabrics, rags, towels];
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true
+};
 export class BusinessComponent extends Component {
   render() {
     const { t } = this.props;
     return (
       <section id="business">
-        <img alt="laundry" src={galoo} />
         <div className="container">
           <div className="row">
             <div className="mb-3 col-12">
@@ -18,7 +29,24 @@ export class BusinessComponent extends Component {
             </div>
           </div>
           <div className="row">
-            <div className="col-12">
+            <div className="col-6">
+              <div className="slide-container">
+                <Slide {...properties}>
+                  {slideImages.map(slide => (
+                    <div className="each-slide">
+                      <div
+                        style={{
+                          backgroundImage: `url(${slide})`,
+                          height: '300px',
+                          backgroundSize: 'cover'
+                        }}
+                      />
+                    </div>
+                  ))}
+                </Slide>
+              </div>
+            </div>
+            <div className="col-6">
               <div className="business-message">
                 <p>{t('business.description')}</p>
                 <h5>{t('business.description.tip')}</h5>
